ed23a1317cc13c45522beddaf1b6dfff185a799a
Manel <msoler92@gmail.com>
2024-01-27 19:36:32 +0100

    Added OpenAPI support

--- src/main/java/cat/itacademy/barcelonactiva/solereina/manel/s05/t01/n02/controllers/ControllerExceptionHandler.java
+++ src/main/java/cat/itacademy/barcelonactiva/solereina/manel/s05/t01/n02/controllers/ControllerExceptionHandler.java
@@ -1,17 +1,18 @@
 package cat.itacademy.barcelonactiva.solereina.manel.s05.t01.n02.controllers;
 
-import cat.itacademy.barcelonactiva.solereina.manel.s05.t01.n02.model.dto.FlowerDTO;
 import cat.itacademy.barcelonactiva.solereina.manel.s05.t01.n02.model.exceptions.IdNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(IdNotFoundException.class)
-    protected ResponseEntity<FlowerDTO> handleIdNotFound() {
-        return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    protected ResponseEntity<String> handleIdNotFound(IdNotFoundException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
     }
 }
